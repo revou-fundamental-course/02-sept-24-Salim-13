434cbfac810cf88a6e9a3abcf590b6f8f02146fa
Salim-13 <salim.farsi13@gmail.com>
2024-09-09 23:11:22 +0700

    luas sgitiga

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function hitung(){
+    let panjangAlas = document.getElementById("panjang-alas").value;
+    let tinggi = document.getElementById("tinggi").value;
+    let hasil = parseInt(panjangAlas) + parseInt(tinggi);
+
+    console.log(hasil);
+
+
+}
