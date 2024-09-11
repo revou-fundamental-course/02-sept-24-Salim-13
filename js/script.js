586975cd3c190c4e0f414fb766cbeed4f161313f
Salim-13 <salim.farsi13@gmail.com>
2024-09-11 07:26:15 +0700

    luas+keliling

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 function hitung(){
     let panjangAlas = document.getElementById("panjang-alas").value;
     let tinggi = document.getElementById("tinggi").value;
-    let hasil = parseInt(panjangAlas) + parseInt(tinggi);
+    let hasil = (1/2) * parseInt(panjangAlas) * parseInt(tinggi);
 
     console.log(hasil);
+}
 
+function hitungKeliling(){
+    let sisiA = document.getElementById("sisi-a").value;
+    let sisiB = document.getElementById("sisi-b").value;
+    let sisiC = document.getElementById("sisi-c").value;
 
+    let keliling= parseInt(sisiA) + parseInt(sisiB) +parseInt(sisiC);
+    console.log(keliling);
 }
